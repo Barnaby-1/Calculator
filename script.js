fcb70b0b7a6e41294408cfe179b0f29913c29773
Barnaby Hamilton <barnabyhamilton@outlook.com>
2024-08-03 15:31:22 +0100

    add clear display and assign first number and first operator

--- script.js
+++ script.js
@@ -16,18 +16,18 @@ const divide = (num1, num2) => {
     return num1 / num2;
 }
 
-let firstNum = 0;
-let firstOperator;
-let secondNum = 0;
+let firstNum = null;
+let firstOperator = null;
+let secondNum = null;
 
 const operate = (operator, num1, num2) => {
     if (operator === '+') {
         add(num1, num2);
     } else if (operator === '-') {
         subtract(num1, num2);
-    } else if (operator === '*') {
+    } else if (operator === '×') {
         multiply(num1, num2);
-    } else if (operator === '/') {
+    } else if (operator === '÷') {
         divide(num1, num2);
     }
 }
@@ -40,9 +40,39 @@ const operators = document.getElementsByClassName('operator');
 
 const buttonClicks = () => {
     for (let i = 0; i < buttons.length; i++) {
+        let buttonText = buttons[i].textContent;
         buttons[i].addEventListener('click', () => {
-            output.textContent = buttons[i].textContent;
+            if (buttons[i].className.includes('number')) {
+                if (firstNum === null && secondNum === null) {
+                    firstNum = '';
+                    secondNum = '';
+                }
+                // when a number is clicked assign firstNum to the textContent of the button and display the number
+                firstNum += buttonText;
+                console.log(firstNum);
+                output.textContent = firstNum;
+            } else if (
+                // assign firstOperator to the textContent of the button that was clicked
+                // and if the textContent includes one of the operators listed
+                buttonText.includes('÷') ||
+                buttonText.includes('×') ||
+                buttonText.includes('-') ||
+                buttonText.includes('+') ||
+                buttonText.includes('=')) {
+                    firstOperator = buttonText;
+                    console.log(firstOperator);
+            } else if (buttonText === 'AC') {
+                clearDisplay();
+            }
         })
     }
 }
 buttonClicks();
+
+
+const clearDisplay = () => {
+    output.textContent = '0';
+    firstNum = null;
+    firstOperator = null;
+    secondNum = null;
+}
